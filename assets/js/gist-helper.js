70db931395576e9fd344d8d1dd87a7601ed2aa0b
Eduardo cruz <pamecoaxaca.telcel@gmail.com>
2025-11-28 03:18:57 -0600

    Crear gist-helper.js

---
+++ assets/js/gist-helper.js
@@ -0,0 +1,7 @@
+/* gist-helper.js
+ * Pequeño wrapper reutilizable para subir/descargar JSON a GitHub Gist con backoff
+ * Requiere: idb y config guardada en store 'config' key='gist' con {id, token}
+ */
+(async function(){
+  // Este fichero es opcional. El evidence-module contiene ya la función.
+})();
